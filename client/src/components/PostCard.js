051f745f3770522201c26cdb876519530a8bb9f0
Manohar <manohar.solleti@gmail.com>
2020-11-17 17:34:24 +0530

    add single post and delete post

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import { Button, Label, Icon, Card, Image } from 'semantic-ui-react';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
   const { user } = useContext(AuthContext);
- 
 
   return (
     <Card fluid>
@@ -35,16 +35,7 @@ function PostCard({
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && (
-          <Button
-            as="div"
-            color="red"
-            floated="right"
-            onClick={() => console.log('Delete post')}
-          >
-            <Icon name="trash" style={{ margin: 0 }} />
-          </Button>
-        )}
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
